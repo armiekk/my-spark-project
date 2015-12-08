bf08256fab095baca7c18b63ae2e8ecb92f007aa
armst0910 <nongarmza@gmail.com>
2015-12-08 23:17:30 +0700

    fixed port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,20 @@ import com.google.gson.*;
 
 public class Main {
     public static void main(String[] args){
+        port(getHerokuAssignedPort());
+
         get("/", (req, res) -> "Hello World");
 
         get("/products", (req, res) -> new Gson().toJson(new Products().getAll()));
+
+
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
--- src/main/java/com/model/Products.java
+++ src/main/java/com/model/Products.java
@@ -7,7 +7,6 @@ package com.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.sql.*;
 import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
